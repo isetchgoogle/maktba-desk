0c8682f4dab150e72bd814c3432b93b9c221b723
3bouda <abdelaliiheb@gmail.com>
2021-11-15 12:20:41 +0100

    tables interface done

--- main.py
+++ main.py
@@ -16,17 +16,53 @@ class MainWindow(QMainWindow):
         #self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         #self.centralwidget.setGraphicsEffect(self.shadow)
         self.setWindowTitle("fuck")
-        self.reserve_btn.clicked.connect(lambda:self.stackedWidget.setCurrentWidget(self.reserve_page))
+        self.crud_btn.clicked.connect(lambda:self.stackedWidget.setCurrentWidget(self.CRUD_page))
         self.emprunte_btn.clicked.connect(lambda:self.stackedWidget.setCurrentWidget(self.emprunte_page))
+        self.reserve_btn.clicked.connect(lambda:self.stackedWidget.setCurrentWidget(self.reserve_page))
         self.menu_btn.clicked.connect(self.menu)
+        self.crud()
+        self.emprunt()
+        self.resrve()
     
+    
+    def crud(self):
+        self.CRUD_table.setColumnWidth(0,100)
+        self.CRUD_table.setColumnWidth(1,150)
+        self.CRUD_table.setColumnWidth(2,100)
+        self.CRUD_table.setColumnWidth(3,100)
+        self.CRUD_table.setColumnWidth(4,100)
+
+
+
+    
+    
+    def emprunt(self):
+        self.emprunte_table.setColumnWidth(0,100)
+        self.emprunte_table.setColumnWidth(1,150)
+        self.emprunte_table.setColumnWidth(2,100)
+        self.emprunte_table.setColumnWidth(3,100)
+        self.emprunte_table.setColumnWidth(4,100)
+        
+    
+    def resrve(self):
+        self.reserve_table.setColumnWidth(0,100)
+        self.reserve_table.setColumnWidth(1,150)
+        self.reserve_table.setColumnWidth(2,100)
+        self.reserve_table.setColumnWidth(3,100)
+        self.reserve_table.setColumnWidth(4,100)
+
+
+
 
     def menu(self):
         if self.left_menu_frame.minimumWidth()==60:
             self.left_menu_frame.setMinimumSize(200,200)
         else:
             self.left_menu_frame.setMinimumSize(60,0)
-        
+
+
+
+
 
 
 app = QApplication(sys.argv)
@@ -36,4 +72,5 @@ try:
     sys.exit(app.exec_())
 except:
     print("Exiting")
+        
 
