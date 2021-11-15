39fb29453bf835a6da38e8ad3f027b9bf31cf071
3bouda <contact.isetchggl@gmail.com>
2021-11-15 09:42:45 +0100

    menu done

--- main.py
+++ main.py
@@ -13,8 +13,20 @@ class MainWindow(QMainWindow):
         super (MainWindow, self).__init__()
         loadUi("interface.ui" , self)
         apply_stylesheet(app, theme='dark_cyan.xml')
-       # self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        #self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        #self.centralwidget.setGraphicsEffect(self.shadow)
+        self.setWindowTitle("fuck")
+        self.reserve_btn.clicked.connect(lambda:self.stackedWidget.setCurrentWidget(self.reserve_page))
+        self.emprunte_btn.clicked.connect(lambda:self.stackedWidget.setCurrentWidget(self.emprunte_page))
+        self.menu_btn.clicked.connect(self.menu)
+    
 
+    def menu(self):
+        if self.left_menu_frame.minimumWidth()==60:
+            self.left_menu_frame.setMinimumSize(200,200)
+        else:
+            self.left_menu_frame.setMinimumSize(60,0)
+        
 
 
 app = QApplication(sys.argv)
